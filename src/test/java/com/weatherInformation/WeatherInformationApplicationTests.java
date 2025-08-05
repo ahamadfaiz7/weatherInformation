b51311dab8b7b1f14f65b7a50438e7ba74322c8b
Mohammad Faiz Ahamad <ahamad.faiz007@gmail.com>
2025-08-05 16:01:43 +1200

    Initial commit with .gitignore

---
+++ src/test/java/com/weatherInformation/WeatherInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherInformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
